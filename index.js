af5150df3efd7ba3d499c9fa7c895ad9fae53fb6
Max <tutv95@gmail.com>
2019-04-22 00:12:45 +0700

    test

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.delete('/todos/:id', todo.delete)
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`App is listening on port PORT...`)
+    console.log(`App is listening on port ${PORT}...`)
 })
