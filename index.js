c72f0e8027a5813c13cd681c75617a89e6a99435
Max <tutv95@gmail.com>
2019-04-22 00:04:57 +0700

    UPdated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-    res.send('Hello world.')
+    res.send('Hello, world!')
 })
 
 const todo = require('./todo')
--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 const Mongoose = require('mongoose')
 
-const MONGO_PATH = process.env.MONGO_PATH || 'localhost'
+const MONGO_PATH = process.env.MONGO_PATH || '127.0.0.1'
 const MONGO_PORT = process.env.MONGO_PORT || 27017
 
 const mongoUri = `mongodb://${MONGO_PATH}:${MONGO_PORT}/hello`
